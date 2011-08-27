466faace082428bac4b3b669ab6f36eba609d729
JordanaCayres <jocayres2@gmail.com>
2011-08-27 02:42:15 -0300

    prontuario 1.1

---
+++ Codigo/prontuario1.1/src/domainModel/Usuario.java
@@ -0,0 +1,49 @@
+package domainModel;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="autenticacao")
+public class Usuario {
+
+    //Atributos Usuario autenticacao
+	
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="idAutenticacao")
+	private int idUser;
+	
+	@Column(name="usuario")
+	private String usuario;
+	
+	@Column(name="senha")
+	private String senha;
+        
+        //Motodos Usuario autenticacao
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
